0f233ab031b981ec6b6bf57d5b2966098535c8bd
Louisaflor-mcm <132604269+Louisaflor-mcm@users.noreply.github.com>
2024-11-21 18:58:04 +0000

    saving changes

---
+++ yulelog/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+       <div>
+        <h1>Project Yule Log </h1>
+       (Yule understand logical engineering, let's officiate games)
+       </div>
+       
+      </main>
+      
+    </div>
+  );
+}
