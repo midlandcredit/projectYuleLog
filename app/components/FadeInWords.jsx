f250b040b2d44e519d0070d00535fdbe5b326707
Louisaflor-mcm <132604269+Louisaflor-mcm@users.noreply.github.com>
2024-11-21 23:49:31 +0000

    changed items in package.json files

--- app/components/FadeInWords.jsx
+++ app/components/FadeInWords.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+'use client';
+import React, { useState, useEffect, useRef } from 'react';
 
-export default function FadeInWords() {
+export default function FadeInWords({text}) {
+  const words = text
+  const [visibleWords, setVisibleWords] = useState([]);
+  const targetDivRef = useRef(null);
+  let topCss = 120;
+
+  const handleCssScroll = () => {
+    if (targetDivRef.current) {
+      // console.log('not sure if i am targeting the div if i am scrolling: ', targetDivRef.current.style)
+      targetDivRef.current.style.top = Math.max(topCss - 0.7*window.scrollY, 0) + "px";
+    }
+  };
+
+  useEffect(() => {
+    let timeoutIds = [];
+    words.forEach((word, index) => {
+      const timeoutId = setTimeout(() => {
+        setVisibleWords((prev) => [...prev, word]);
+      }, index * 300); // Adjust the delay as needed
+      timeoutIds.push(timeoutId);
+    });
+
+    return () => {
+      timeoutIds.forEach(clearTimeout);
+    };
+  }, [words]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleCssScroll);
+  }, []);
+
+
   return (
     <div>
       
