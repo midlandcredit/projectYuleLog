7c68d895f6d25e3e9bd0d5f86dd281ac439591b9
Louisaflor-mcm <132604269+Louisaflor-mcm@users.noreply.github.com>
2024-11-21 22:23:17 +0000

    deleted yulelog folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
       keyframes: {
         fadeIn: {
           '0%': { opacity: '0', transform: 'translateY(10px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' },
+          '100%': { opacity: '1', transform: 'translateY(300px)' },
         }
       },
     },
